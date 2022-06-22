34088448c8684f30893bd9373e8390e0a1cd3b96
DAP <dpetritsch@gmail.com>
2022-06-22 14:55:37 +1000

    Initial Commit

---
+++ SourceControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControl
+//
+//  Created by Dustin Petritsch on 22/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Dustin Petritsch on 22/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
